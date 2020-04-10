7a72acf4c43b24a428e0bbd70bc89bcf264c21f2
das747 <40293014+das747@users.noreply.github.com>
2020-04-10 20:41:29 +0700

    register form

--- data/users.py
+++ data/users.py
@@ -1,7 +1,7 @@
 import datetime
 import sqlalchemy
 from sqlalchemy import orm
-
+from werkzeug.security import generate_password_hash, check_password_hash
 from .db_session import SqlAlchemyBase
 
 
@@ -22,3 +22,9 @@ class User(SqlAlchemyBase):
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                       default=datetime.datetime.now)
     jobs = orm.relation("Jobs", back_populates='user')
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- register.py
+++ register.py
@@ -1,18 +1,57 @@
+from flask import Flask, render_template, redirect
+from flask_wtf.form import FlaskForm
+from wtforms import StringField, PasswordField, IntegerField, SubmitField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+from data.users import User
 from data import db_session
-from data.jobs import Jobs
-from datetime import datetime
+
+db_session.global_init("db/jobs.sqlite")
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-db_session.global_init('db/jobs.sqlite')
+class RegisterForm(FlaskForm):
+    email = EmailField('Login / email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    repeat_password = PasswordField('Repeat password', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired()])
+    position = StringField('Position', validators=[DataRequired()])
+    speciality = StringField('Speciality', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
-session = db_session.create_session()
-job = Jobs()
-job.team_leader = 1
-job.job = 'deployment of residential modules 1 and 2'
-job.collaborators = '2, 3'
-job.work_size = 15
-job.start_date = datetime.now()
-job.is_finished = False
-session.add(job)
 
-session.commit()
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.repeat_password.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+                name=form.name.data,
+                email=form.email.data,
+                surname=form.surname.data,
+                address=form.address.data,
+                position=form.position.data,
+                age=form.age.data,
+                speciality=form.speciality.data
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/register')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
